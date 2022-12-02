b5e5065f528f0786298a052dbd97ad8d99bdcaae
Hector <hmreluz@gmail.com>
2022-12-02 16:26:50 -0500

    Move settings conf to a class

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """Store settings for the game"""
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
