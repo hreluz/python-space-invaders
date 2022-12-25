9c9fb18ed827db15956af96b38251005ff64717e
Hector <hmreluz@gmail.com>
2022-12-25 12:08:39 -0500

    Display the score on game

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,7 @@ class Scoreboard:
 
     def __init__(self, ai_game):
         """Initialize score-keeping attributes"""
-        self.screen = ai_game
+        self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
